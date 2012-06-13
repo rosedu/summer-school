d5da15f2e351c756cf107c02310db4bad9b6e2fc
Diana Mincu <diana.mincu@cti.pub.ro>
2012-06-13 12:31:41 +0300

    Basic main

---
+++ pygame-projects/tetris/main.py
@@ -0,0 +1,60 @@
+import pygame
+import random
+import tetris
+import brick
+from pygame.locals import.*
+
+class GameException(Exception):
+    """
+    Useful for exiting
+    """
+    pass
+
+class Game(object):
+    """
+    Main class
+    """
+
+    def __init__(self, settings = Settings()):
+        pygame.init()
+        self.init_from_settings(settings)
+        self.clock = pygame.time.Clock()
+
+    def init_from_settings(self, settings):
+        """
+        Initializes game from Settings in tetris.py
+        """
+
+        # Initializes screen.
+        self.screen = pygame.display.set_mode(settings.resolution)
+        pygame.display.set_caption(settings.title)
+        pygame.mouse.set_visible(settings.mouse_enabled)
+
+        # Initializes background.
+        background = pygame.Surface(self.screen.get_size())
+        self.background = background.convert()
+        self.background.fill(settings.background)
+
+    def run(self):
+        """
+        Runs the game
+        """
+
+        while(True):
+            try:
+                self.game_tick()
+            except GameException:
+                return
+
+    def game_tick(self):
+        """
+        Handle events and redraw scene
+        """
+        self.clock.tick(60)
+        
+        #Check events.
+
+        #Redraw.
+        self.screen.blit(self.background, (0,0))
+        self.allsprites.draw(self.screen)
+        pygame.display.flip()
