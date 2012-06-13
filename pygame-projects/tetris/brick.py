6a611d95fdbf3c10ac3387bd62296dc6dc95bc73
Teodora Baluta <teobaluta@gmail.com>
2012-06-13 16:45:50 +0300

    Added brick functionality.

--- pygame-projects/tetris/brick.py
+++ pygame-projects/tetris/brick.py
@@ -73,13 +73,13 @@ class Brick(pygame.sprite.Sprite):
 
 class Redbrick(Brick):
     def ___init__(self, x, y, surface):
-        Brick.__init__(self, resources.images.redbrick, x, y, surface)
+        Brick.__init__(self, resources.load_image('redbrick'), x, y, surface)
 
 class Greenbrick(Brick):
     def __init__(self, x, y, surface):
-        Brick.__init__(self, resources.images.greenbrick, x, y, surface)
+        Brick.__init__(self, resources.load_image('greenbrick'), x, y, surface)
 
 class Bluebrick(Brick):
     def __init__(self, x, y, surface):
-        Brick.__init__(self, resources.images.bluebrick, x, y, surface)
+        Brick.__init__(self, resources.load_image('bluebrick'), x, y, surface)
 
