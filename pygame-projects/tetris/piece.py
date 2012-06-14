81a3519569e989b81de63825b01258a2f578f4c3
Student CS <student@mjolnir.labs.cs.pub.ro>
2012-06-14 10:01:26 +0300

    Fixed more bugs.

--- pygame-projects/tetris/piece.py
+++ pygame-projects/tetris/piece.py
@@ -1,15 +1,15 @@
-import pygame, os
+import pygame,  os
 from pygame.locals import *
 
 #current_id = 0
+pygame.display.set_mode()
 
 class Piece(pygame.sprite.Sprite):
     """
     Movable piece with arrows
     Return: piece object
     Methods: """
-    # Width = Height = L
-    L = 15
+    # Width = Height = 15
 
     def __init__(self, type_of_piece):
         ''' (x, y) = initialpos
@@ -31,24 +31,25 @@ class Piece(pygame.sprite.Sprite):
         """
         if type_of_piece == 1: #I
             self.image = pygame.image.load(os.path.join('data','tealbrick.png')).convert()
-            self.starting_pos = [Rect(50,-10,L,L), Rect(65,-10,L,L), Rect(80,-10,L,L), Rect(95, -10, L, L)]
-            self.display_pos = [Rect(200,95,L,L), Rect(215,95,L,L), Rect(230,95,L,L), Rect(245, 95, L, L)]
+            self.starting_pos = [Rect(50,-10,15,15),
+			    Rect(65,-10,15,15), Rect(80,-10,15,15), Rect(95, -10, 15, 15)]
+            self.display_pos = [Rect(200,95,15,15), Rect(215,95,15,15), Rect(230,95,15,15), Rect(245, 95, 15, 15)]
             self.type = 'I'
         
         if type_of_piece == 2: #S
             self.image = pygame.image.load(os.path.join('data', 'redbrick.png')).convert()
-            self.starting_pos = [Rect(95,-10,L,L), Rect(80,-10,L,L), Rect(80,5,L,L), Rect(65,5,L,L)]
-            self.display_pos = [Rect(245,95,L,L), Rect(230,95,L,L), Rect(230,110,L,L), Rect(215,110,L,L)]
+            self.starting_pos = [Rect(95,-10,15,15), Rect(80,-10,15,15), Rect(80,5,15,15), Rect(65,5,15,15)]
+            self.display_pos = [Rect(245,95,15,15), Rect(230,95,15,15), Rect(230,110,15,15), Rect(215,110,15,15)]
             self.type = 'S'
         if type_of_piece == 3: #Z
             self.image = pygame.image.load(os.path.join('data', 'greenbrick.png')).convert()
-            self.starting_pos = [Rect(65,-10,L,L), Rect(80,-10,L,L), Rect(80,5,L,L), Rect(95,5,L,L)]
-            self.display_pos = [Rect(215,95,L,L), Rect(230,95,L,L), Rect(230,110,L,L), Rect(245,110,L,L)]
+            self.starting_pos = [Rect(65,-10,15,15), Rect(80,-10,15,15), Rect(80,5,15,15), Rect(95,5,15,15)]
+            self.display_pos = [Rect(215,95,15,15), Rect(230,95,15,15), Rect(230,110,15,15), Rect(245,110,15,15)]
             self.type = 'Z'
         if type_of_piece == 4: #T 
             self.image = pygame.image.load(os.path.join('data', 'purplebrick.png')).convert()
-            self.starting_pos = [Rect(80,-10,L,L), Rect(65,5,L,L), Rect(80,5,L,L), Rect(95,5,L,L)]
-            self.display_pos = [Rect(230,95,L,L), Rect(215,110,L,L), Rect(230,110,L,L), Rect(245,110,L,L)]
+            self.starting_pos = [Rect(80,-10,15,15), Rect(65,5,15,15), Rect(80,5,15,15), Rect(95,5,15,15)]
+            self.display_pos = [Rect(230,95,15,15), Rect(215,110,15,15), Rect(230,110,15,15), Rect(245,110,15,15)]
             self.type = 'T'
 
     def coord(self):
@@ -65,7 +66,7 @@ class Piece(pygame.sprite.Sprite):
         if self.counter < time_passed:
             self.counter = time_passed + speed
             for pos in self.starting_pos:
-                pos.top += L
+                pos.top += 15
         
     def move_right(self, grid):
         for pos in self.starting_pos:
@@ -76,7 +77,7 @@ class Piece(pygame.sprite.Sprite):
                     if pos.right == ii.left and pos.top == ii.top:
                         return      
         for pos in self.starting_pos:
-            pos.right +=L
+            pos.right +=15
         
     def move_left(self, grid):
         for pos in self.starting_pos:
@@ -87,7 +88,7 @@ class Piece(pygame.sprite.Sprite):
                     if pos.left == ii.right and pos.top == ii.top:
                         return          
         for pos in self.starting_pos:
-            pos.right -=L
+            pos.right -=15
    
     def not_ok(self, pos, old_pos):
         if pos.right > 155 or pos.top < 5 or pos.left < 5:
@@ -100,18 +101,18 @@ class Piece(pygame.sprite.Sprite):
         # type 'I' can rotate in 2 ways
         if self.type == 'I':
             if self.state == 1:
-                self.starting_pos[0] = Rect(self.starting_pos[2].left, self.starting_pos[2].top-2*L,L,L)
-                self.starting_pos[1] = Rect(self.starting_pos[2].left, self.starting_pos[2].top-L,L,L)
-                self.starting_pos[3] = Rect(self.starting_pos[2].left, self.starting_pos[2].top+L,L,L)
+                self.starting_pos[0] = Rect(self.starting_pos[2].left, self.starting_pos[2].top-2*15,15,15)
+                self.starting_pos[1] = Rect(self.starting_pos[2].left, self.starting_pos[2].top-15,15,15)
+                self.starting_pos[3] = Rect(self.starting_pos[2].left, self.starting_pos[2].top+15,15,15)
                 for pos in self.starting_pos:
                     if not_ok(pos, old_pos):
                         return
                 self.state = 2
                 return
             if self.state == 2:
-                self.starting_pos[0] = Rect(self.starting_pos[2].left-2*L, self.starting_pos[2].top,L,L)
-                self.starting_pos[1] = Rect(self.starting_pos[2].left-L, self.starting_pos[2].top,L,L)
-                self.starting_pos[3] = Rect(self.starting_pos[2].left+L, self.starting_pos[2].top,L,L)
+                self.starting_pos[0] = Rect(self.starting_pos[2].left-2*15, self.starting_pos[2].top,15,15)
+                self.starting_pos[1] = Rect(self.starting_pos[2].left-15, self.starting_pos[2].top,15,15)
+                self.starting_pos[3] = Rect(self.starting_pos[2].left+15, self.starting_pos[2].top,15,15)
                 for pos in self.starting_pos:
                     if not_ok(pos, old_pos):
                         return
@@ -120,18 +121,18 @@ class Piece(pygame.sprite.Sprite):
         # type 'S' rotations
         if self.type == 'S':
             if self.state == 1:
-                self.starting_pos[0] = Rect(self.starting_pos[1].left, self.starting_pos[1].top-L,L,L)
-                self.starting_pos[2] = Rect(self.starting_pos[1].left+L, self.starting_pos[1].top,L,L)
-                self.starting_pos[3] = Rect(self.starting_pos[1].left+L, self.starting_pos[1].top+L,L,L)
+                self.starting_pos[0] = Rect(self.starting_pos[1].left, self.starting_pos[1].top-15,15,15)
+                self.starting_pos[2] = Rect(self.starting_pos[1].left+15, self.starting_pos[1].top,15,15)
+                self.starting_pos[3] = Rect(self.starting_pos[1].left+15, self.starting_pos[1].top+15,15,15)
                 for pos in self.starting_pos:
                     if not_ok(pos, old_pos):
                         return
                 self.state = 2
                 return
             if self.state == 2:
-                self.starting_pos[0] = Rect(self.starting_pos[1].left+L, self.starting_pos[1].top,L,L)
-                self.starting_pos[2] = Rect(self.starting_pos[1].left, self.starting_pos[1].top+L,L,L)
-                self.starting_pos[3] = Rect(self.starting_pos[1].left-L, self.starting_pos[1].top+L,L,L)
+                self.starting_pos[0] = Rect(self.starting_pos[1].left+15, self.starting_pos[1].top,15,15)
+                self.starting_pos[2] = Rect(self.starting_pos[1].left, self.starting_pos[1].top+15,15,15)
+                self.starting_pos[3] = Rect(self.starting_pos[1].left-15, self.starting_pos[1].top+15,15,15)
                 for pos in self.starting_pos:
                     if not_ok(pos, old_pos):
                         return
